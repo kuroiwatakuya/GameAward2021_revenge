e6a4e245e0b3d7d6f79d5bcd53845e38fb0a3622
nanasesakamoto <mknn797.gmail.com>
2021-05-24 18:36:20 +0900

    坂本：タイトル画面実装、UI点滅処理の追加

--- GameAward2021_revenge/Assets/nanase/TitleManager.cs
+++ GameAward2021_revenge/Assets/nanase/TitleManager.cs
@@ -2,16 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour
 {
+    [SerializeField] private GameObject TitlestartUI;
+    private Image image_start;
+
+    private float alpha;
+    private float aspeed;
+
     void Start()
     {
-
+        alpha = 1.0f;
+        aspeed = -0.5f;
+        image_start = TitlestartUI.GetComponent<Image>();
     }
 
     void Update()
     {
+        if (alpha <= 0.2)
+            aspeed = 0.5f;
+        if(alpha >= 1.1)
+            aspeed = -0.5f;
+
+        alpha += aspeed * Time.deltaTime;
+        image_start.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown("joystick button 0"))
         {
             SceneManager.LoadScene("Select");
